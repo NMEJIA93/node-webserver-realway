f3b200b8de5ddd0159897207c406f6595a653064
NMEJIA <norbeymejiacortes@gmail.com>
2023-04-21 16:49:17 -0500

    webServerListo

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const port = process.env.PORT;
 
 //Handlebars 
 app.set('view engine', 'hbs');
-hbs.registerPartials(__dirname + '/views/partials', function (err) {});
+hbs.registerPartials(__dirname + '/views/partials', function (err) { });
 
 
 
@@ -18,34 +18,37 @@ hbs.registerPartials(__dirname + '/views/partials', function (err) {});
 // servir contenido statico || carpeta publica 
 app.use(express.static('Public'))
 
-app.get('/',  (req, res) => {
-    res.render('home',{
+app.get('/', (req, res) => {
+    res.render('home', {
         nombre: 'Norbey Mejia',
         titulo: 'Curso de Node'
     });
 })
 
 
-app.get('/generic',  (req, res) => {
-    res.render('generic',{
+app.get('/generic', (req, res) => {
+    res.render('generic', {
         nombre: 'Norbey Mejia',
         titulo: 'Curso de Node'
     });
 })
 
-app.get('/elements',  (req, res) => {
-    res.render('elements',{
+app.get('/elements', (req, res) => {
+    res.render('elements', {
         nombre: 'Norbey Mejia',
         titulo: 'Curso de Node'
     });
 })
 
 
-app.get('*',  (req, res) => {
+app.get('*', (req, res) => {
     res.sendFile(__dirname + '/Public/404.html');
 })
 
+app.listen(port, () => {
+    console.log(`el servidor esta escuchando por el puerto ${port}`)
+})
 
-console.log(`el servidor esta escuchando por el puerto ${port}`)
+
 
 app.listen(port)
